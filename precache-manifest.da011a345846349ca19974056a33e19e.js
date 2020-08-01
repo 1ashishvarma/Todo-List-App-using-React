73cdbf886b551cc5a0e0591af7c1e7597d6565e3
1ashishvarma <ashish.roczzz007@gmail.com>
2020-08-01 15:52:21 +0530

    Updates

---
+++ precache-manifest.da011a345846349ca19974056a33e19e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4266d87e13ded4b3e0d485d86ed8c4",
+    "url": "/Todo-List-App-using-React/index.html"
+  },
+  {
+    "revision": "e0051f2ca29d3b46ff80",
+    "url": "/Todo-List-App-using-React/static/css/main.e39c0fa4.chunk.css"
+  },
+  {
+    "revision": "8bcc985b5b940f4de42d",
+    "url": "/Todo-List-App-using-React/static/js/2.6f7a44b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List-App-using-React/static/js/2.6f7a44b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0051f2ca29d3b46ff80",
+    "url": "/Todo-List-App-using-React/static/js/main.f3211900.chunk.js"
+  },
+  {
+    "revision": "20d52d461f9d397c9711",
+    "url": "/Todo-List-App-using-React/static/js/runtime-main.e29d2f77.js"
+  }
+]);
